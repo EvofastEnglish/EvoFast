7ef8f819022bdb877da5d695ac1bf2f15f4db970
lieudvq-hisoft <109263957+lieudvq-hisoft@users.noreply.github.com>
2025-02-27 09:20:02 +0700

    Update QuestionAttemptDto.cs

--- EvoFast.Application/Dtos/QuestionAttemptDto.cs
+++ EvoFast.Application/Dtos/QuestionAttemptDto.cs
@@ -3,6 +3,6 @@ namespace EvoFast.Application.Dtos;
 public record QuestionAttemptDto(
     bool IsBookmarked,
     Guid WordSetAttemptId,
+    Guid Id,
     QuestionDto Question
-    // WordSetAttemptDto WordSetAttempt
     );
