7f5e71ca9d25c8962223688bb483ed4958099fe0
doriandang <193190651+lieudvq0302@users.noreply.github.com>
2025-05-10 16:43:34 +0700

    Feat(@AI): update b.HasIndex("UserId", "AiTestId")

---
+++ EvoFast.Infrastructure/Migrations/20250510094209_Add_UniqueIndex_To_AiTestResult.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EvoFast.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Add_UniqueIndex_To_AiTestResult : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_AiTestResults_UserId",
+                table: "AiTestResults");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AiTestResults_UserId_AiTestId",
+                table: "AiTestResults",
+                columns: new[] { "UserId", "AiTestId" },
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_AiTestResults_UserId_AiTestId",
+                table: "AiTestResults");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AiTestResults_UserId",
+                table: "AiTestResults",
+                column: "UserId");
+        }
+    }
+}
