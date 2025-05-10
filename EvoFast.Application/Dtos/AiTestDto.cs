189515ad930346be528573f6096f37a17c5614b1
doriandang <193190651+lieudvq0302@users.noreply.github.com>
2025-05-10 15:07:05 +0700

    Feat(@AI): update table AiTest

--- EvoFast.Application/Dtos/AiTestDto.cs
+++ EvoFast.Application/Dtos/AiTestDto.cs
@@ -4,5 +4,6 @@ public record AiTestDto(
     Guid Id,
     string Title,
     string Description,
+    string DescriptionFinish,
     List<AiTestSectionDto> AiTestSections
     );
---
+++ EvoFast.Infrastructure/Migrations/20250510080024_Add_DescriptionFinish_To_AiTest_Table.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EvoFast.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Add_DescriptionFinish_To_AiTest_Table : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "DescriptionFinish",
+                table: "AiTests",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DescriptionFinish",
+                table: "AiTests");
+        }
+    }
+}
