8ee8ef811c95d01c3575229f85329f304141e0a2
doriandang <lieudvq0302@gmail.com>
2025-05-18 09:51:58 +0700

    refactor: delete AiTestSectionsController

--- EvoFast.Application/AiTestSections/Commands/StartAiTestSection/StartAiTestSectionCommand.cs
+++
@@ -1,8 +0,0 @@
-using EvoFast.Application.Dtos;
-
-namespace EvoFast.Application.AiTestSections.Commands.StartAiTestSection;
-
-public record StartAiTestSectionCommand(StartAiTestSectionRequest StartAiTestSectionRequest) : ICommand<StartAiTestSectionResult>;
-
-public record StartAiTestSectionRequest(Guid AiTestSessionId, Guid AiTestSectionId, Guid QuestionId);
-public record StartAiTestSectionResult(List<AiTestChatMessageDto>  ChatMessageDtos);
--- EvoFast.Application/AiTestSections/Queries/GetAiTestSectionResult/GetAiTestSectionResultQuery.cs
+++
@@ -1,6 +0,0 @@
-using EvoFast.Application.Dtos;
-
-namespace EvoFast.Application.AiTestSections.Queries.GetAiTestSectionResult;
-
-public record GetAiTestSectionResultQuery(Guid AiTestSectionId) : IQuery<GetAiTestSectionResultResult>;
-public record GetAiTestSectionResultResult(AiTestSectionResultDto AiTestSectionResult);
